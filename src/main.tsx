9c91fcba1b0bd0f25245a8e8e85a9e9a4820e4da
Samuel Gómez <sagomadev@gmail.com>
2023-10-22 11:55:13 +0200

    Modularice provider in a plugin folder

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
 
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { TanStackProvider } from "./plugins/TanStackProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <TanStackProvider>
       <NextUIProvider>
         <main className="dark text-foreground bg-background">
           <RouterProvider router={router} />
         </main>
       </NextUIProvider>
-    </QueryClientProvider>
+    </TanStackProvider>
   </React.StrictMode>
 );
