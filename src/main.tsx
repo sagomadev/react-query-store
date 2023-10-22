5e145e76fd909bea27cbedea5d5b31b44023876f
Samuel Gómez <sagomadev@gmail.com>
2023-10-22 11:53:21 +0200

    Add react-query an basic config provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
 
-import './index.css';
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={router} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
