92aacf080e2bbd681cbb92ab2a48e5816ce4b2d6
Samuel Gómez <sagomadev@gmail.com>
2023-10-23 19:33:31 +0200

    add axios and services

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,14 +1,13 @@
+export { productsApi } from "./api/productsApi";
 
+export { ProductCard } from "./components/ProductCard";
+export { ProductList } from "./components/ProductList";
 
+export type { Product } from "./interfaces/product";
 
-export { ProductCard } from './components/ProductCard';
-export { ProductList } from './components/ProductList';
+export { StoreLayout } from "./layout/StoreLayout";
 
-
-export { StoreLayout } from './layout/StoreLayout';
-
-
-export { CompleteListPage } from './pages/CompleteListPage';
-export { MensPage } from './pages/MensPage';
-export { NewProduct } from './pages/NewProduct';
-export { WomensPage } from './pages/WomensPage';
+export { CompleteListPage } from "./pages/CompleteListPage";
+export { MensPage } from "./pages/MensPage";
+export { NewProduct } from "./pages/NewProduct";
+export { WomensPage } from "./pages/WomensPage";
