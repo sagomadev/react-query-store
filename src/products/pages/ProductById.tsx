c1dbd5b0a2388b1934a7b3bb30a496ef3385fcaa
Samuel Gómez <sagomadev@gmail.com>
2023-10-30 17:58:16 +0100

    Scroll Top on visit detail product

--- src/products/pages/ProductById.tsx
+++ src/products/pages/ProductById.tsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom";
 import { ProductCard, useProduct } from "..";
+import { useEffect } from "react";
 
 export const ProductById = () => {
   const { id } = useParams();
 
   const { product, isLoading } = useProduct({ id: +id! });
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="flex-col">
