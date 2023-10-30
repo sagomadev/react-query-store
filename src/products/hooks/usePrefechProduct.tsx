fc7aea179735cded8f2defc0653fed101b0f1031
Samuel Gómez <sagomadev@gmail.com>
2023-10-30 18:23:02 +0100

    Add Prefech Product Hook

---
+++ src/products/hooks/usePrefechProduct.tsx
@@ -0,0 +1,13 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { productActions } from "..";
+
+export const usePrefechProduct = () => {
+  const queryClient = useQueryClient();
+  const prefetchProduct = (id: number) => {
+    queryClient.prefetchQuery({
+      queryKey: ["product", id],
+      queryFn: () => productActions.getProductById(id),
+    });
+  };
+  return prefetchProduct;
+};
