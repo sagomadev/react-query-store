95e9b5836e9bb4c4da31b5b1e1c2147d35968aca
Samuel Gómez <sagomadev@gmail.com>
2023-11-02 19:57:13 +0100

    Invalidate query to refresh fetch

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,9 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { productActions } from "..";
 
 export const useProductMutation = () => {
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["products", { filterKey: data.category }],
+      });
+    },
   });
 
   return mutation;
