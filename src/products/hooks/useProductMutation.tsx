775a78bfb6bec205f0595f929eee81e5fde955d4
Samuel Gómez <sagomadev@gmail.com>
2023-11-02 20:35:47 +0100

    create product with optimistic pattern

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -5,6 +5,19 @@ export const useProductMutation = () => {
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
+    onMutate: (product) => {
+      const optimisticProduct = { id: Math.random(), ...product };
+
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: product.category }],
+        (old) => {
+          if (old) {
+            return [...old, optimisticProduct];
+          }
+          return [optimisticProduct];
+        }
+      );
+    },
     // onSuccess: (product) => {
     //   queryClient.invalidateQueries({
     //     queryKey: ["products", { filterKey: product.category }],
